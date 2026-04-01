4899bf9f5e396f7ecbec53e809feda957cd3a631
constanzavicencio <constanzavicencioc@gmail.com>
2026-04-01 11:16:44 -0300

    Subir archivos

---
+++ constantes.py
@@ -0,0 +1,7 @@
+# Puedes (y debes) cambiar el comando de "python3" al que corresponda a tu sistema
+# operativo. Por ejemplo, "python", "py", "python3.12".
+COMANDO_PYTHON = "python"
+
+# Tiempo de espera entre cada evento.
+# Para la corrección final será de 0.2 segundos como tope máximo.
+TIEMPO_ENTRE_EVENTOS = 0.2
